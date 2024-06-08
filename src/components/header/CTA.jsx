a9d5035391aab6c8eadcb84e7169d6fe56631de3
kivaghani <kartikvaghani@rapideaseinnovations.com>
2024-06-08 14:04:25 +0530

    update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import CV from'../../assets/cv.pdf'
+import React from 'react';
+import CV from '../../assets/cv.pdf';
 
 const CTA = () => {
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = CV;
+    link.download = 'cv.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'> Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      <button onClick={handleDownload} className='btn'>Download CV</button>
+      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
-  )
+  );
 }
 
-export default CTA
+export default CTA;
